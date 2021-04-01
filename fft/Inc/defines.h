d0492a270fd8324c3daaf0554a342acea78eb4ff
Ouajih Safae <safae.ouajih44@gmail.com>
2021-04-01 23:11:30 +0200

    _

---
+++ fft/Inc/defines.h
@@ -0,0 +1,15 @@
+/*
+ * defines.h
+ *
+ *  Created on: Apr 1, 2021
+ *      Author: safae
+ */
+
+#ifndef INC_DEFINES_H_
+#define INC_DEFINES_H_
+
+
+#define ARM_MATH_CM7
+
+#define __FPU_PRESENT            1      /*!< FPU present                                  */
+#endif /* INC_DEFINES_H_ */
